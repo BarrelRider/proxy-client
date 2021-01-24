bccc9d6ab821362c4d5a4802ffad40c5acb1eff7
suatogme <suat.ogme@gmail.com>
2021-01-25 01:17:01 +0300

    changeOrigin added to access the heroku app

--- server.js
+++ server.js
@@ -7,17 +7,25 @@ require("dotenv").config({
   path: path.join(__dirname, `.env/.env.${process.env.NODE_ENV}`),
 });
 
-const proxy = httpProxy.createProxyServer();
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const API = process.env.API;
 const app = express();
-
 app.use(bodyParser.json());
+
+
+const proxy = httpProxy.createProxyServer();
+
+
+proxy.on('proxyRes', (proxyRes, req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+});
+
+
 app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("/api/*", (req, res) => {
-  proxy.web(req, res, { target: API }, (e) => {
+  proxy.web(req, res, { target: API, changeOrigin: true }, (e) => {
     console.log(e.message);
     return res.status(500).send({
       error: true,
@@ -33,9 +41,7 @@ app.all("/*", (req, res, next) => {
   });
 });
 
-// const httpServer = http.createServer(app);
-// httpServer.listen(PORT);
-
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
+  console.log('API: ', API)
 });
